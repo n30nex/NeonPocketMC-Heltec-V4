3e7be00ccac61ddf63d20f205fbad99633c5140f
n30nex <blushellneon@outlook.com>
2026-09-21 00:16:16 -0400

    Complete native solar test runner and manifest packaging

--- test/test_solar_companion/test_solar_companion.cpp
+++ test/test_solar_companion/test_solar_companion.cpp
@@ -55,3 +55,8 @@ TEST(SolarCompanion, CountersSaturateAndWakeRemainsFinite) {
   EXPECT_LE(SolarCompanion::recoverySeconds, 60);
   EXPECT_GT(SolarCompanion::resumeMv, SolarCompanion::cutoffMv);
 }
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
